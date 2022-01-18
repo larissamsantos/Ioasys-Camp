76dc4d6ad74e0172f48a3bc2f4dfaa0a01768f81
Larissa <larissa.menezes.santos@gmail.com>
2022-01-18 08:39:57 -0300

    .

--- Módulo 3 - Javascript Avançado/READme.md
+++ Módulo 3 - Javascript Avançado/READme.md
@@ -1,6 +1,15 @@
 **Módulo 3 - Javascript Avançado**
 
-O desafio foi reduzir o número de ifs dentro de uma função onde diversas condições precisam ser atendidas. Para isso, foram utilizados dois métodos:
+O desafio foi reduzir o número de ifs dentro de uma função onde diversas condições precisam ser atendidas. 
+Foram utilizados os pacotes [readline-sync](https://www.npmjs.com/package/readline-sync) para pedir os inputs do usuário e [lodash](https://www.npmjs.com/package/lodash) para comparação dos objetos Map. Os pacotes podem ser instalados rodando os seguintes comandos no terminal:
 
+```
+npm install readline-sync
+npm i -g npm
+npm i --save lodash
+```
+
+Foram criadas três funções para checar se os dados fornecidos pelo usuário correspondem aos dados presentes no "banco de dados". Cada função tem o seguinte funcionamento:
 * Simplesmente unir todas as condições necessárias em apenas um if utilizando o operador lógico &&
-* Criar dois objetos Map e compará-los. Nesse exemplo foi utilizado o comando JSON.stringify, porém também poderia-se utilizar o comando Lodash isEqual. Mais informações sobre esses comandos podem ser encontradas [aqui](https://www.samanthaming.com/tidbits/33-how-to-compare-2-objects/). Além disso, como apenas um boolean é utilizado nesse caso, também fiz uso do operador ternário ? : ao invés de um condicional if.
+* Criar dois objetos Map e compará-los. Nesse exemplo foi utilizado o comando Lodash isEqual, porém também poderia-se utilizar o comando JSON.stringify. Mais informações sobre esses comandos podem ser encontradas [aqui](https://www.samanthaming.com/tidbits/33-how-to-compare-2-objects/). Além disso, como apenas um boolean é utilizado nesse caso, também fiz uso do operador ternário ? : ao invés de um condicional if.
+* Utilizar o método [every()](https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array/every) para comparar os elementos do array do banco de dados e do array fornecido pelo usuário, além da propriedade [length](https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array/length) para garantir que ambos os arrays tem o mesmo tamanho.
--- Módulo 3 - Javascript Avançado/codigo-hadouken-melhorado.js
+++ Módulo 3 - Javascript Avançado/codigo-hadouken-melhorado.js
@@ -1,40 +1,67 @@
-const nome = "Larissa Menezes";
+const readlineSync = require('readline-sync');
+var _ = require('lodash');
+
+//Dados de entrada
+const nome = "larissa menezes";
 const idade = 27;
-const nacionalidade = "Brasileira";
-const estado = "Sergipe";
-const cidade = "Aracaju";
-const formacao = "ioasysCamp";
+const nacionalidade = "brasileira";
+const estado = "sergipe";
+const cidade = "aracaju";
+const formacao = "ioasyscamp";
+
+const dadosCadastradosMap = new Map();
+dadosCadastradosMap.set('nome', nome);
+dadosCadastradosMap.set('idade', idade);
+dadosCadastradosMap.set('nacionalidade', nacionalidade);
+dadosCadastradosMap.set('estado', estado);
+dadosCadastradosMap.set('cidade', cidade);
+dadosCadastradosMap.set('formacao', formacao);
+
+const dadosCadastradosArray = [nome, idade, nacionalidade, estado, cidade, formacao];
 
 //Primeira forma: utilizar o operador lógico &&
-
 const cadastroUsuario1 = () => {
-    if (nome === "Larissa Menezes" && idade === 27 && nacionalidade === "Brasileira" && estado === "Sergipe" && cidade === "Aracaju" && formacao === "ioasysCamp") {
+    if (nome === nomeUsuario && idade === idadeUsuario && nacionalidade === nacionalidadeUsuario && estado === estadoUsuario && cidade === cidadeUsuario && formacao === formacaoUsuario) {
         console.log("Cadastrada!");
     } else console.log("Não cadastrada!");
 }
 
 //Segunda forma: utilizar o objeto Map e operador ternário ? :
 
-const meusDados = new Map();
-meusDados.set('nome', 'Larissa Menezes');
-meusDados.set('idade', 27);
-meusDados.set('nacionalidade', 'Brasileira');
-meusDados.set('estado', 'Sergipe');
-meusDados.set('cidade', 'Aracaju');
-meusDados.set('formacao', 'ioasysCamp');
-
-const dadosCadastrados = new Map();
-dadosCadastrados.set('nome', 'Larissa Menezes');
-dadosCadastrados.set('idade', 27);
-dadosCadastrados.set('nacionalidade', 'Brasileira');
-dadosCadastrados.set('estado', 'Sergipe');
-dadosCadastrados.set('cidade', 'Aracaju');
-dadosCadastrados.set('formacao', 'ioasysCamp');
-
-const cadastroUsuario2 = () => {
-    const status = (JSON.stringify(meusDados) === JSON.stringify(dadosCadastrados)) ? "Cadastrada!" : "Não cadastrada";
+const cadastroUsuario2 = (dadosUsuario,dadosCadastrados) => {
+    const status = (_.isEqual(dadosUsuario,dadosCadastrados)) ? "Cadastrada!" : "Não cadastrada";
     console.log(status);
 }
 
+//Terceira forma: utilizar a função every e comparar os arrays
+const cadastroUsuario3 = (dadosCadastrados, dadosUsuario) => {
+    if (dadosCadastrados.length === dadosUsuario.length && dadosCadastradosArray.every((value, index) => value === dadosUsuarioArray[index])) {
+        console.log("Cadastrada!");
+    } else console.log("Não cadastrada!");
+}
+
+
+//Obtendo os inputs do usuário
+
+const nomeUsuario = readlineSync.question('Digite seu nome: ').toLowerCase();
+const idadeUsuario = Number(readlineSync.question('Digite sua idade: '));
+const nacionalidadeUsuario = readlineSync.question('Digite sua nacionalidade: ').toLowerCase();
+const estadoUsuario = readlineSync.question('Digite seu estado: ').toLowerCase();
+const cidadeUsuario = readlineSync.question('Digite sua cidade: ').toLowerCase();
+const formacaoUsuario = readlineSync.question('Digite sua formacao: ').toLowerCase();
+const dadosUsuarioArray = [nomeUsuario, idadeUsuario, nacionalidadeUsuario, estadoUsuario, cidadeUsuario, formacaoUsuario];
+
+const dadosUsuarioMap = new Map();
+dadosUsuarioMap.set('nome', nomeUsuario);
+dadosUsuarioMap.set('idade', idadeUsuario);
+dadosUsuarioMap.set('nacionalidade', nacionalidadeUsuario);
+dadosUsuarioMap.set('estado', estadoUsuario);
+dadosUsuarioMap.set('cidade', cidadeUsuario);
+dadosUsuarioMap.set('formacao', formacaoUsuario);
+
+console.log(dadosUsuarioMap);
+console.log(dadosCadastradosMap);
+
 cadastroUsuario1();
-cadastroUsuario2();
+cadastroUsuario2(dadosUsuarioMap, dadosCadastradosMap);
+cadastroUsuario3(dadosCadastradosArray, dadosUsuarioArray);
